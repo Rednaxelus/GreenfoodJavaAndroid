912834ca85175ed27cdf940ab89e49cd9dca1925
ismael094 <ismaelpitufo@gmail.com>
2019-11-14 21:40:34 +0000

    Added password field to enterprise table

--- app/src/main/java/com/example/greenfoodjava/database/EnterpriseTable.java
+++ app/src/main/java/com/example/greenfoodjava/database/EnterpriseTable.java
@@ -11,6 +11,7 @@ public class EnterpriseTable extends SQLiteOpenHelper {
     private static final String ID = "ID";
     private static final String NAME = "name";
     private static final String NIF = "nif";
+    private static final String PASSWORD = "password";
     private static final String DESCRIPTION = "description";
     private static final String PHONE_NUMBER = "phoneNumber";
     private static final String ADDRESS = "address";
@@ -24,7 +25,8 @@ public class EnterpriseTable extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TABLE_NAME
-                + " (ID INTEGER PRIMARY KEY, " + NAME + " TEXT DEFAULT ' ', " + NIF + " TEXT DEFAULT ' ', "
+                + " (ID INTEGER PRIMARY KEY, " + NAME + " TEXT DEFAULT ' ',"
+                + PASSWORD + " TEXT DEFAULT ' ', " + NIF + " TEXT DEFAULT ' ', "
                 + DESCRIPTION + " TEXT DEFAULT ' ', " + PHONE_NUMBER + " TEXT DEFAULT ' ', "
                 + ADDRESS + " TEXT DEFAULT ' ')";
         db.execSQL(createTable);
@@ -40,17 +42,19 @@ public class EnterpriseTable extends SQLiteOpenHelper {
      * New entry in user table.
      * @param name name
      * @param nif nif
+     * @param password password
      * @param description description
      * @param phoneNumber phoneNumber
      * @param address address
      * @return true//false
      */
-    boolean addData(String name, String nif, String description, String phoneNumber,String address) {
+    boolean addData(String name, String nif, String password, String description, String phoneNumber,String address) {
         SQLiteDatabase sqlDB = this.getWritableDatabase();
 
         ContentValues contentValues = new ContentValues();
         contentValues.put(NAME, name);
         contentValues.put(NIF, nif);
+        contentValues.put(PASSWORD, password);
         contentValues.put(NAME, description);
         contentValues.put(PHONE_NUMBER, phoneNumber);
         contentValues.put(ADDRESS, address);

3a91c35c77306adb020be3fca5b6932a43b2697d
adripsau <alv_morenitu@hotmail.com>
2019-11-17 13:04:14 +0000

    Login process method added without logic

--- app/src/main/java/com/example/greenfoodjava/ui/login/UserRegistrationActivity.java
+++ app/src/main/java/com/example/greenfoodjava/ui/login/UserRegistrationActivity.java
@@ -1,11 +1,15 @@
 package com.example.greenfoodjava.ui.login;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.greenfoodjava.R;
 
@@ -19,5 +23,23 @@ public class UserRegistrationActivity extends AppCompatActivity {
 
     public void goToEnterpriseRegistration(View v){
         startActivity(new Intent(UserRegistrationActivity.this, EnterpriseRegistrationActivity.class));
+    }
+
+    public void loginProcess(View v){
+        if (userExist(v)){
+            TextView editText = findViewById(R.id.username);
+            editText.setText("Email already exist");
+        }else {
+
+        }
+
+    }
+
+    private boolean userExist(View v) {
+        EditText usernameEditText = findViewById(R.id.username);
+        String username = usernameEditText.getText().toString();
+        EditText passwordEditText = findViewById(R.id.password);
+        String password = passwordEditText.getText().toString();
+        return true;
     }
 }

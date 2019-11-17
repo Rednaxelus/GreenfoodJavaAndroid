9ab1f39a685e7c18e2712862b234304ab302fd39
adsfk <alv_morenitu@hotmail.com>
2019-11-17 21:37:06 +0000

    Register as enterprise or restaurant logic done

--- app/src/main/java/com/example/greenfoodjava/ui/login/EnterpriseRegistrationActivity.java
+++ app/src/main/java/com/example/greenfoodjava/ui/login/EnterpriseRegistrationActivity.java
@@ -10,8 +10,12 @@ import android.widget.Switch;
 
 import com.example.greenfoodjava.R;
 
+import java.util.regex.Pattern;
+
 public class EnterpriseRegistrationActivity extends Activity {
     private boolean isSwitchChecked;
+    private boolean fieldError = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,19 +34,123 @@ public class EnterpriseRegistrationActivity extends Activity {
     }
 
     public void register(View view) {
-        EditText editText = findViewById(R.id.name);
-        String message = editText.getText().toString();
-        EditText edit2 = findViewById(R.id.address);
-        if (isSwitchChecked) {
-            edit2.setText("LOLOLO");
-        } else {
-            edit2.setText("NICE");
-        }
-        if (message.length() == 0) {
-            editText.setText("Está feo");
-            return;
+        fieldError = false;
+        checkEmailField();
+        checkPassField();
+        checkNifField();
+        checkDescriptionField();
+        checkPhoneField();
+        checkAddressField();
+        if (!fieldError) {
+            createEnterprise();
         }
     }
+
+    private void checkAddressField() {
+        EditText addressText = findViewById(R.id.address);
+        String address = addressText.getText().toString();
+        if (address.length() < 5 || address.length() > 25) {
+            addressText.setError("Address doesn't exists");
+            fieldError = true;
+        }
+    }
+
+    private void checkPhoneField() {
+        EditText phoneText = findViewById(R.id.phone);
+        String phone = phoneText.getText().toString();
+        if (phone.length() != 9) {
+            phoneText.setError("Phone doesn't exists");
+            fieldError = true;
+        } else if (!Pattern.matches("(\\d{9})", phone)) {
+            phoneText.setError("Phone doesn't exists");
+            fieldError = true;
+        }
+    }
+
+    private void checkDescriptionField() {
+        EditText descText = findViewById(R.id.description);
+        if (descText.getText().toString().length() > 75) {
+            descText.setError("Description too long");
+            fieldError = true;
+        }
+    }
+
+    private void checkNifField() {
+        EditText nifText = findViewById(R.id.nif);
+        String nif = nifText.getText().toString();
+        if (nif.length() != 9) {
+            nifText.setError("Wrong nif");
+            fieldError = true;
+        } else if (!Pattern.matches("(\\d{8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke])", nif)) {
+            nifText.setError("Wrong nif");
+            fieldError = true;
+        }
+    }
+
+    private void checkPassField() {
+        EditText passText = findViewById(R.id.password);
+        if (passText.getText().toString().length() <= 6) {
+            passText.setError("Password too short");
+            fieldError = true;
+        }
+        if (passText.getText().toString().length() > 50) {
+            passText.setError("Password too long");
+            fieldError = true;
+        }
+    }
+
+    private void checkEmailField() {
+        EditText emailText = findViewById(R.id.name);
+        String email = emailText.getText().toString();
+        if (!Pattern.matches("(\\w|-)+@\\w+.(com|es)", email)) {
+            emailText.setError("Email doesn't exists");
+            fieldError = true;
+        } else if (email.length() <= 3) {
+            emailText.setError("Email too short");
+            fieldError = true;
+        } else if (email.length() > 50) {
+            emailText.setError("Email too long");
+            fieldError = true;
+        } else if (emailExists(email)) {
+            emailText.setError("Email already exists");
+            fieldError = true;
+        }
+
+    }
+
+    private boolean emailExists(String email) {
+        if (email.equals("Existe")) {
+            return true;
+        }
+        return false;
+    }
+
+    private void createEnterprise() {
+        EditText text = findViewById(R.id.name);
+        String name = text.getText().toString();
+
+        text = findViewById(R.id.password);
+        String pass = text.getText().toString();
+
+        text = findViewById(R.id.nif);
+        String nif = text.getText().toString();
+
+        text = findViewById(R.id.description);
+        String description = text.getText().toString();
+
+        text = findViewById(R.id.phone);
+        String phone = text.getText().toString();
+
+        text = findViewById(R.id.address);
+        String address = text.getText().toString();
+
+        if (isSwitchChecked) {
+            System.out.println("Aqui se crea el restaurante bro");
+        } else {
+            System.out.println("Aqui se crea la empresa bro");
+        }
+    }
+
 
     public void goBack(View view) {
         startActivity(new Intent(this, LoginActivity.class));
